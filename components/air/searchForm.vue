47ad4680375e191b935295f9a16110f47d440bd4
SamzoGuo <53573565+SamzoGuo@users.noreply.github.com>
2019-10-15 16:09:45 +0800

    自定义验证表单

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -40,7 +40,13 @@
 
       <el-form-item label="出发时间">
         <!-- change 用户确认选择日期时触发 -->
-        <el-date-picker type="date" placeholder="请选择日期" style="width: 100%;" @change="handleDate"></el-date-picker>
+        <el-date-picker
+          type="date"
+          placeholder="请选择日期"
+          style="width: 100%;"
+          @change="handleDate"
+          v-model="form.departDate"
+        ></el-date-picker>
       </el-form-item>
 
       <el-form-item label>
@@ -54,6 +60,7 @@
 </template>
 
 <script>
+import moment from "moment";
 export default {
   data() {
     return {
@@ -115,8 +122,8 @@ export default {
       //   }
 
       if (this.cities.length === 0) return;
-      this.form[type+'City'] = this.cities[0].value;
-      this.form[type+'Code'] = this.cities[0].sort;
+      this.form[type + "City"] = this.cities[0].value;
+      this.form[type + "Code"] = this.cities[0].sort;
     },
 
     // 目标城市输入框获得焦点时触发
@@ -138,13 +145,41 @@ export default {
     },
 
     // 确认选择日期时触发
-    handleDate(value) {},
+    handleDate(value) {
+      this.form.departDate = moment(value).format("YYYY-MM-DD");
+    },
 
     // 触发和目标城市切换时触发
     handleReverse() {},
 
     // 提交表单是触发
-    handleSubmit() {}
+    handleSubmit() {
+      //自定义验证
+      const rules = {
+        //message是错误信息，value是表单中的值
+        departCity: { message: "请输入出发城市", value: this.form.departCity },
+        destCity: { message: "请输入到达城市", value: this.form.destCity },
+        departDate: { message: "请选择出发时间", value: this.form.departDate }
+      };
+      //循环rules这个对象，判断对象属性的value如果是空的，打印出message错误信息
+      let valid = true;
+      Object.keys(rules).forEach(v => {
+        //只要有一次验证不通过，后台验证不用执行
+        if (!valid) return;
+        const { message, value } = rules[v];
+        //对象属性的value如果是空的
+        if (!value) {
+          this.$message.error(message);
+          //验证不通过
+          valid = false;
+        }
+      });
+      if(!valid) return
+      this.$router.push({
+          path:'/air/flights',
+          query:this.form
+      })
+    }
   },
   mounted() {}
 };
