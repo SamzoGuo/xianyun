cd67930b4ba086cb183e8bf1282721be2d8b1831
SamzoGuo <53573565+SamzoGuo@users.noreply.github.com>
2019-10-16 08:29:47 +0800

    座位信息部分显示与隐藏的控制

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flight-item">
-    <div>
+    <div  @click="isShow=!isShow">
       <!-- 显示的机票信息 -->
-      <el-row type="flex" align="middle" class="flight-info">
+      <el-row type="flex" align="middle" class="flight-info" >
         <el-col :span="6">
           <span>{{item.airline_name}}</span>
           {{item.flight_no}}
@@ -28,7 +28,7 @@
         </el-col>
       </el-row>
     </div>
-    <div class="flight-recommend">
+    <div class="flight-recommend" v-if="isShow">
       <!-- 隐藏的座位信息列表 -->
       <el-row type="flex" justify="space-between" align="middle">
         <el-col :span="4">低价推荐</el-col>
@@ -60,6 +60,11 @@
 
 <script>
 export default {
+     data(){
+      return {
+          isShow:false
+      }
+  },
   // props除了可以使用数组之外，还可以使用对象
   props: {
     // item是声明组件可以接受item属性
