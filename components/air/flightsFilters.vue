9fd21c4e6489a77b6595c8cb8522ecc5eba8db9e
SamzoGuo <53573565+SamzoGuo@users.noreply.github.com>
2019-10-17 19:33:08 +0800

    时间段的条件过滤

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -90,7 +90,16 @@ export default {
 
     // 选择出发时间时候触发
     handleFlightTimes(value) {
-      console.log(value);
+        //根据value过滤列表，只保留当前符合条件的机票列表
+        //  数组中第一项是开始时间，第二项是终止时间
+        const arr1=value.split(',') // [6, 12]
+        const arr2=this.data.flights.filter(v=>{
+            // 出发时间的小时
+            const start=+v.dep_time.split(':')[0]
+            // 比较航班出发时间是否在选中的时间段内
+            return start>=arr1[0]&&start<arr1[1]
+        })
+       this.$emit("setDataList",arr2);
     },
 
     // 选择航空公司时候触发
