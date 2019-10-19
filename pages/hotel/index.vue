f265e7567e43c5727ec357301054c35ddbc7a9e1
SamzoGuo <53573565+SamzoGuo@users.noreply.github.com>
2019-10-19 19:13:19 +0800

    试用高德地图

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-      酒店
+    <H3>高德地图</H3>
+    <div id="container"></div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  mounted() {
+    window.onLoad = function() {
+      var map = new AMap.Map("container");
+    };
+    var url =
+      "https://webapi.amap.com/maps?v=1.4.15&key=a68bc8339009bf16b324cd00f678bae8&callback=onLoad";
+    var jsapi = document.createElement("script");
+    jsapi.charset = "utf-8";
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+#container {
+  margin:0 auto;
+  width: 1000px;
+  height:500px
+}
 </style>
